ead97afedbd9ba1aff2b1e10ab67b53ca45df40b
ChristopheBardoux <christophe.bardoux@gmail.com>
2020-03-27 17:55:40 +0700

    I dont have cheese

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-
 class Character(object):
     class_name = 'Character'
 
-    def __init__(self, position=(0.0, 0.0)):
+    def __init__(self, attack, position=(0.0, 0.0)):
         self.health = 100
         self.stamina = 100
         self.position = position
-
-    @classmethod
-    def get_character_class(cls):
-        return cls.class_name
+        self.attack = attack
 
     def __del__(self):
-        print 'I\'m dying!!!'
+        # print 'I\'m dying!!!'
+        pass
 
     def report(self):
         print 'Health: %i\nStamina: %i\nPosition: (%f,%f)' % \
@@ -21,42 +18,53 @@ class Character(object):
     def move(self, offset):
         self.position = (self.position[0] + offset[0], self.position[1] + offset[1])
 
-    def attack(self, enemy):
-        pass
-
 
 class Player(Character):
     class_name = 'Player'
 
-    def __init__(self, name, position=(0.0, 0.0)):
-        super(Player, self).__init__(position=position)
+    def __init__(self, name, attack, position=(0.0, 0.0)):
+        super(Player, self).__init__(attack=attack, position=position)
         self.name = name
 
     def __del__(self):
         super(Player, self).__del__()
-        print 'My name was %s.' % self.name
+        # print 'My name was %s.' % self.name
+        pass
 
     def report(self):
         print 'Name: %s' % self.name
         super(Player, self).report()
 
 
-class Npc(Character):
-    class_name = 'Npc'
+def fight(char1, char2):
+    count = 0
+    while char1.health > 0 and char2.health > 0:
+        count += 1
+        char1.health -= char2.attack
+        char2.health -= char1.attack
+        print 'round ' + str(count) + ' / ' + char1.name + ' health : ' + str(
+            char1.health) + ' / ' + char2.name + ' health : ' + str(char2.health)
+
+    if char1.health <= 0 and char1.health <= 0:
+        print "Draw"
+    elif char1.health <= 0:
+        print char2.name + ' won'
+    elif char2.health <= 0:
+        print char1.name + ' won'
+
 
 
 def main():
-    player = Player('Covid')
-    enemy = Npc()
-
-    enemy.move((2.5, 4.5))
-    enemy.report()
-
-    player.move((2.5, 5.5))
-    player.report()
+    fighter1 = Player('Covid', 40)
+    fighter2 = Player('Human', 20)
+    fight(fighter1, fighter2)
 
 
+'''
+    fighter1.move((2.5, 4.5))
+    fighter1.report()
+    fighter2.move((2.5, 5.5))
+    fighter2.report()
+'''
 main()
 print 'Finished!'
-
-
