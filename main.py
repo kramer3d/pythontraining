48a1a01bc12a94e7df0ac4120331f7991a34353f
ChristopheBardoux <christophe.bardoux@gmail.com>
2020-03-30 17:56:22 +0700

    Weapon/Armors dictionary

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-Weapons = {
-    'fist': 5,
-    'knife': 20,
-    'gun': 50
-}
+Items = {
+    'weapons': {
+        'fist': {
+            'damage': 5
+        },
+        'knife': {
+            'damage': 20
+        },
+        'gun': {
+            'damage': 50
+        },
 
-Armors = {
-    'unarmored': 0,
-    'armor1': 10,
-    'armor2': 20,
-    'armor3': 40
+    },
+    'armors': {
+        'unarmored': {
+            'defense': 0
+        },
+        'armor1': {
+            'defense': 10
+        },
+        'armor2': {
+            'defense': 20
+        },
+        'armor3': {
+            'defense': 40
+        },
+    }
 }
 
 
@@ -19,6 +35,23 @@ def clamp(n, minn, maxn):
         return maxn
     else:
         return n
+
+
+class Weapon(object):
+    def __init__(self, name='fist'):
+        self.name = name
+        self.damage = Items['weapons'][self.name]['damage']
+
+
+class Armor(object):
+    def __init__(self, name='unarmored'):
+        self.name = name
+        self.defense = Items['armors'][self.name]['defense']
+
+    def deplete_defense(self, val):
+        self.defense -= val
+        if self.defense<=0:
+            self.defense=0
 
 
 class Character(object):
@@ -44,13 +77,13 @@ class Character(object):
 class Fighter(Character):
     class_name = 'Fighter'
 
-    def __init__(self, name, weapon='fist', armor='unarmored', position=(0.0, 0.0)):
+    def __init__(self, name, weapon=Weapon(), armor=Armor(), position=(0.0, 0.0)):
         super(Fighter, self).__init__(position=position)
         self.name = name
         self.weapon = weapon
         self.armor = armor
-        self.attack = Weapons[self.weapon]
-        self.defense = Armors[self.armor]
+        self.attack = weapon.damage
+        self.defense = armor.defense
 
     def __del__(self):
         super(Fighter, self).__del__()
@@ -60,15 +93,19 @@ class Fighter(Character):
     def report(self):
         print 'Name: %s' % self.name
         # super(Fighter, self).report()
-        print 'Fight with : %s / damage : %i / defense : %i' % (self.weapon, self.attack,self.defense)
+        print 'Fight with: %s / damage: %i // Armor: %s / defense: %i' % (
+            self.weapon.name, self.attack, self.armor.name, self.defense)
 
 
 def fight(char1, char2):
     count = 0
     while char1.health > 0 and char2.health > 0:
         count += 1
-        char1.health -= clamp(char2.attack - char1.defense, 0, 100)
-        char2.health -= clamp(char1.attack - char2.defense, 0, 100)
+        char1.health -= clamp(char2.attack - char1.armor.defense, 0, 100)
+        char1.armor.deplete_defense(char2.attack)
+        char2.health -= clamp(char1.attack - char2.armor.defense, 0, 100)
+        char2.armor.deplete_defense(char1.attack)
+
         print 'round ' + str(count) + ' : ' + char1.name + ' health : ' + str(
             char1.health) + ' / ' + char2.name + ' health : ' + str(char2.health)
 
@@ -81,8 +118,11 @@ def fight(char1, char2):
 
 
 def main():
-    fighter1 = Fighter('Covid', 'knife','armor3')
-    fighter2 = Fighter('Human', 'gun')
+    knife1 = Weapon('knife')
+    armor1 = Armor('armor3')
+    fighter1 = Fighter('Covid', knife1, armor1)
+    gun1 = Weapon('gun')
+    fighter2 = Fighter('Human',gun1)
     fighter1.report()
     fighter2.report()
     fight(fighter1, fighter2)
