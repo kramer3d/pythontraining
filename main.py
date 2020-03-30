0d4943e00a8dc852e32b558f1d11f4ae5750bc86
ChristopheBardoux <christophe.bardoux@gmail.com>
2020-03-30 11:28:59 +0700

    Weapon dictionary

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+Weapons = {
+    'fist': 5,
+    'knife': 20,
+    'gun': 50
+}
+
+
 class Character(object):
     class_name = 'Character'
 
-    def __init__(self, attack, position=(0.0, 0.0)):
+    def __init__(self, attack=0, weapon='fist', position=(0.0, 0.0)):
         self.health = 100
         self.stamina = 100
         self.position = position
-        self.attack = attack
+        # self.attack = attack
+        self.weapon = weapon
 
     def __del__(self):
         # print 'I\'m dying!!!'
@@ -22,9 +30,10 @@ class Character(object):
 class Player(Character):
     class_name = 'Player'
 
-    def __init__(self, name, attack, position=(0.0, 0.0)):
-        super(Player, self).__init__(attack=attack, position=position)
+    def __init__(self, name, attack=0, weapon='fist', position=(0.0, 0.0)):
+        super(Player, self).__init__(attack=attack, weapon=weapon, position=position)
         self.name = name
+        self.attack = Weapons[self.weapon]
 
     def __del__(self):
         super(Player, self).__del__()
@@ -33,7 +42,8 @@ class Player(Character):
 
     def report(self):
         print 'Name: %s' % self.name
-        super(Player, self).report()
+        # super(Player, self).report()
+        print 'Fight with : %s / damage : %i' % (self.weapon, Weapons[self.weapon])
 
 
 def fight(char1, char2):
@@ -42,10 +52,10 @@ def fight(char1, char2):
         count += 1
         char1.health -= char2.attack
         char2.health -= char1.attack
-        print 'round ' + str(count) + ' / ' + char1.name + ' health : ' + str(
+        print 'round ' + str(count) + ' : ' + char1.name + ' health : ' + str(
             char1.health) + ' / ' + char2.name + ' health : ' + str(char2.health)
 
-    if char1.health <= 0 and char1.health <= 0:
+    if char1.health <= 0 and char2.health <= 0:
         print "Draw"
     elif char1.health <= 0:
         print char2.name + ' won'
@@ -53,10 +63,11 @@ def fight(char1, char2):
         print char1.name + ' won'
 
 
-
 def main():
-    fighter1 = Player('Covid', 40)
-    fighter2 = Player('Human', 20)
+    fighter1 = Player('Covid', 0, 'knife')
+    fighter2 = Player('Human', 0, 'gun')
+    fighter1.report()
+    fighter2.report()
     fight(fighter1, fighter2)
 
 
